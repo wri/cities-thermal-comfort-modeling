f220eaedfb796c275bb56b4ac7fba0c95e7f372b
Kenn Cartier <kenn.cartier@wri.org>
2024-12-04 13:29:06 -0800

    Added detection of null no_data value

--- worker_manager/config_validator.py
+++ worker_manager/config_validator.py
@@ -178,14 +178,22 @@ def _verify_processing_config(processing_config_df, source_base_path, target_bas
 
                     tif_df, unique_metrics_df = get_parameters_for_custom_tif_files(city_data, tile_folder_name, cif_feature_list)
 
+                    if tif_df['nodata'].isnull().any():
+                        files_with_nan = tif_df.loc[tif_df['nodata'].isnull(), 'filename'].tolist()
+                        files_with_nan_str = ','.join(map(str,files_with_nan))
+                        msg = f"Folder {tile_folder_name} and possibly other folders has forbidden no_data='nan' in file(s) ({files_with_nan_str})."
+                        invalids.append(msg)
+
+                        break
+
                     if unique_metrics_df.shape[0] > 1:
-                        msg = f'TIF files in folder {tile_folder_name} has inconsistent parameters with {unique_metrics_df.shape[0]} unique parameter variants.'
+                        msg = f'TIF files in folder {tile_folder_name} and possibly other folders has inconsistent parameters with {unique_metrics_df.shape[0]} unique parameter variants.'
                         invalids.append(msg)
 
                         msg = f'TIF parameters: {tif_df.to_json(orient='records')}'
                         invalids.append(msg)
 
-                        msg = 'Stopping analysis at first set of inconsistent TIF files'
+                        msg = 'Stopping analysis at first set of inconsistent TIF files.'
                         invalids.append(msg)
 
                         break
@@ -194,6 +202,8 @@ def _verify_processing_config(processing_config_df, source_base_path, target_bas
 
 
 def get_parameters_for_custom_tif_files(city_data, tile_folder_name, cif_feature_list):
+    import sys
+
     tile_folder = os.path.join(city_data.source_city_data_path, city_data.folder_name_primary_source_data,
                                tile_folder_name)
     tif_files = list_files_with_extension(tile_folder, '.tif')
@@ -202,7 +212,8 @@ def get_parameters_for_custom_tif_files(city_data, tile_folder_name, cif_feature
 
     filtered_existing_list = filter_list_by_another_list(tif_files, processing_list)
 
-    tif_df = pd.DataFrame(columns=['filename', 'crs', 'width', 'height', 'resolution', 'compression'])
+    # tif_df = pd.DataFrame(columns=['filename', 'crs', 'width', 'height', 'resolution', 'compression'])
+    tif_df = pd.DataFrame(columns=['filename', 'crs', 'width', 'height', 'nodata'])
     for tif_file in filtered_existing_list:
         tif_file_path = os.path.join(tile_folder, tif_file)
         with rasterio.open(tif_file_path) as dataset:
@@ -210,13 +221,16 @@ def get_parameters_for_custom_tif_files(city_data, tile_folder_name, cif_feature
             crs = dataset.crs.to_string()
             width = dataset.profile["width"]
             height = dataset.profile["height"]
-            resolution = dataset.res
-            compression = dataset.compression
-            new_row = {'filename': tif_file, 'crs': crs, 'width': width, 'height': height, 'resolution': resolution,
-                       'compression': compression}
+            no_data = dataset.nodata if dataset.nodata is not None else ~sys.maxsize
+            # resolution = dataset.res
+            # compression = dataset.compression
+            # new_row = {'filename': tif_file, 'crs': crs, 'width': width, 'height': height, 'nodata': no_data, 'resolution': resolution,
+            #            'compression': compression}
+            new_row = {'filename': tif_file, 'crs': crs, 'width': width, 'height': height, 'nodata': no_data}
             tif_df.loc[len(tif_df)] = new_row
 
-    metrics_df = tif_df[['crs', 'width', 'height', 'resolution', 'compression']]
+    # metrics_df = tif_df[['crs', 'width', 'height', , 'nodata', 'resolution', 'compression']]
+    metrics_df = tif_df[['crs', 'width', 'height']]
     unique_metrics_df = metrics_df.drop_duplicates()
 
     return tif_df, unique_metrics_df

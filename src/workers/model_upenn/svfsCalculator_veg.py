1acb85353b5bfa3cc22c1f16a61707c6845b5860
Kenn Cartier <kenn.cartier@wri.org>
2025-09-28 23:00:40 -0700

    removed passing arrays by reference since it didn't help

--- src/workers/model_upenn/svfsCalculator_veg.py
+++ src/workers/model_upenn/svfsCalculator_veg.py
@@ -10,80 +10,81 @@ import shutil
 from osgeo import gdal
 from osgeo.gdalconst import *
 
+
 def annulus_weight(altitude, aziinterval):
     '''assign different weight to different annulus'''
     n = 90.
-    steprad = (360./aziinterval) * (np.pi/180.)
-    annulus = 91.-altitude
-    w = (1./(2.*np.pi)) * np.sin(np.pi / (2.*n)) * np.sin((np.pi * (2. * annulus - 1.)) / (2. * n))
+    steprad = (360. / aziinterval) * (np.pi / 180.)
+    annulus = 91. - altitude
+    w = (1. / (2. * np.pi)) * np.sin(np.pi / (2. * n)) * np.sin((np.pi * (2. * annulus - 1.)) / (2. * n))
     weight = steprad * w
-    
+
     return weight
 
 
 def saverasternd(gdal_data, filename, raster):
     rows = gdal_data.RasterYSize
     cols = gdal_data.RasterXSize
-    
+
     outDs = gdal.GetDriverByName("GTiff").Create(filename, cols, rows, int(1), GDT_Float32)
     outBand = outDs.GetRasterBand(1)
-    
+
     # write the data
     outBand.WriteArray(raster, 0, 0)
     # flush data to disk, set the NoData value and calculate stats
     outBand.FlushCache()
     outBand.SetNoDataValue(-9999)
-    
+
     # georeference the image and set the projection
     outDs.SetGeoTransform(gdal_data.GetGeoTransform())
     outDs.SetProjection(gdal_data.GetProjection())
 
 
 # def shadowingfunctionglobalradiation(a, azimuth, altitude, scale, dlg, forsvf):
-def shadowingfunctionglobalradiation(a, azimuth, altitude, scale, forsvf, temp):
-    
-    #%This m.file calculates shadows on a DEM
-    #% conversion
-    degrees = np.pi/180.
+def shadowingfunctionglobalradiation(a, azimuth, altitude, scale, forsvf):
+    # %This m.file calculates shadows on a DEM
+    # % conversion
+    degrees = np.pi / 180.
     if azimuth == 0.0:
         azimuth = 0.000000000001
     azimuth = np.dot(azimuth, degrees)
     altitude = np.dot(altitude, degrees)
-    #% measure the size of the image
+    # % measure the size of the image
     sizex = a.shape[0]
     sizey = a.shape[1]
     if forsvf == 0:
         barstep = np.max([sizex, sizey])
-#         dlg.progressBar.setRange(0, barstep)
-    #% initialise parameters
+    #         dlg.progressBar.setRange(0, barstep)
+    # % initialise parameters
     f = a
     dx = 0.
     dy = 0.
     dz = 0.
-    temp[:] = 0
+    temp = np.zeros((sizex, sizey))
     index = 1.
-    #% other loop parameters
+    # % other loop parameters
     amaxvalue = a.max()
-    pibyfour = np.pi/4.
-    threetimespibyfour = 3.*pibyfour
-    fivetimespibyfour = 5.*pibyfour
-    seventimespibyfour = 7.*pibyfour
+    pibyfour = np.pi / 4.
+    threetimespibyfour = 3. * pibyfour
+    fivetimespibyfour = 5. * pibyfour
+    seventimespibyfour = 7. * pibyfour
     sinazimuth = np.sin(azimuth)
     cosazimuth = np.cos(azimuth)
     tanazimuth = np.tan(azimuth)
     signsinazimuth = np.sign(sinazimuth)
     signcosazimuth = np.sign(cosazimuth)
-    dssin = np.abs((1./sinazimuth))
-    dscos = np.abs((1./cosazimuth))
+    dssin = np.abs((1. / sinazimuth))
+    dscos = np.abs((1. / cosazimuth))
     tanaltitudebyscale = np.tan(altitude) / scale
 
-    #% main loop
+    # % main loop
     while (amaxvalue >= dz and np.abs(dx) < sizex and np.abs(dy) < sizey):
-#         if forsvf == 0:
-#             dlg.progressBar.setValue(index)
-    #while np.logical_and(np.logical_and(amaxvalue >= dz, np.abs(dx) <= sizex), np.abs(dy) <= sizey):(np.logical_and(amaxvalue >= dz, np.abs(dx) <= sizex), np.abs(dy) <= sizey):
-        #if np.logical_or(np.logical_and(pibyfour <= azimuth, azimuth < threetimespibyfour), np.logical_and(fivetimespibyfour <= azimuth, azimuth < seventimespibyfour)):
-        if (pibyfour <= azimuth and azimuth < threetimespibyfour or fivetimespibyfour <= azimuth and azimuth < seventimespibyfour):
+        #         if forsvf == 0:
+        #             dlg.progressBar.setValue(index)
+        # while np.logical_and(np.logical_and(amaxvalue >= dz, np.abs(dx) <= sizex), np.abs(dy) <= sizey):(np.logical_and(amaxvalue >= dz, np.abs(dx) <= sizex), np.abs(dy) <= sizey):
+        # if np.logical_or(np.logical_and(pibyfour <= azimuth, azimuth < threetimespibyfour), np.logical_and(fivetimespibyfour <= azimuth, azimuth < seventimespibyfour)):
+        if (
+                pibyfour <= azimuth and azimuth < threetimespibyfour or fivetimespibyfour <= azimuth and azimuth < seventimespibyfour):
             dy = signsinazimuth * index
             dx = -1. * signcosazimuth * np.abs(np.round(index / tanazimuth))
             ds = dssin
@@ -92,84 +93,83 @@ def shadowingfunctionglobalradiation(a, azimuth, altitude, scale, forsvf, temp):
             dx = -1. * signcosazimuth * index
             ds = dscos
 
-        #% note: dx and dy represent absolute values while ds is an incremental value
-        dz = ds *index * tanaltitudebyscale
+        # % note: dx and dy represent absolute values while ds is an incremental value
+        dz = ds * index * tanaltitudebyscale
         temp[0:sizex, 0:sizey] = 0.
         absdx = np.abs(dx)
         absdy = np.abs(dy)
-        xc1 = (dx+absdx)/2.+1.
-        xc2 = sizex+(dx-absdx)/2.
-        yc1 = (dy+absdy)/2.+1.
-        yc2 = sizey+(dy-absdy)/2.
-        xp1 = -((dx-absdx)/2.)+1.
-        xp2 = sizex-(dx+absdx)/2.
-        yp1 = -((dy-absdy)/2.)+1.
-        yp2 = sizey-(dy+absdy)/2.
-        temp[int(xp1)-1:int(xp2), int(yp1)-1:int(yp2)] = a[int(xc1)-1:int(xc2), int(yc1)-1:int(yc2)]-dz
+        xc1 = (dx + absdx) / 2. + 1.
+        xc2 = sizex + (dx - absdx) / 2.
+        yc1 = (dy + absdy) / 2. + 1.
+        yc2 = sizey + (dy - absdy) / 2.
+        xp1 = -((dx - absdx) / 2.) + 1.
+        xp2 = sizex - (dx + absdx) / 2.
+        yp1 = -((dy - absdy) / 2.) + 1.
+        yp2 = sizey - (dy + absdy) / 2.
+        temp[int(xp1) - 1:int(xp2), int(yp1) - 1:int(yp2)] = a[int(xc1) - 1:int(xc2), int(yc1) - 1:int(yc2)] - dz
         # f = np.maximum(f, temp)  # bad performance in python3. Replaced with fmax
         f = np.fmax(f, temp)
         index += 1.
-    
-    f = f-a
+
+    f = f - a
     f = np.logical_not(f)
     sh = np.double(f)
 
     return sh
-    
+
 
 # def shadowingfunction_20(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue, bush, dlg, forsvf):
-def shadowingfunction_20(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue, bush, forsvf,
-                         temp, tempvegdem, tempvegdem2, tempbush, sh, vbshvegsh, vegsh, g):
-    #% This function casts shadows on buildings and vegetation units
-    #% conversion
-    
-    degrees = np.pi/180.
+def shadowingfunction_20(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue, bush, forsvf):
+    # % This function casts shadows on buildings and vegetation units
+    # % conversion
+
+    degrees = np.pi / 180.
     if azimuth == 0.0:
         azimuth = 0.000000000001
     azimuth = np.dot(azimuth, degrees)
     altitude = np.dot(altitude, degrees)
-    #% measure the size of the image
+    # % measure the size of the image
     sizex = a.shape[0]
     sizey = a.shape[1]
-    #% initialise parameters
+    # % initialise parameters
     if forsvf == 0:
         barstep = np.max([sizex, sizey])
-    
+
     dx = 0.
     dy = 0.
     dz = 0.
 
-
     # WRI Note: zero out arrays passed by reference
-    temp[:] = 0
-    tempvegdem[:] = 0
-    tempvegdem2[:] = 0
-    tempbush[:] = 0
-    sh[:] = 0
-    vbshvegsh[:] = 0
-    vegsh[:] = 0
+    temp = np.zeros((sizex, sizey))
+    tempvegdem = np.zeros((sizex, sizey))
+    tempvegdem2 = np.zeros((sizex, sizey))
+    tempbush = np.zeros((sizex, sizey))
+    sh = np.zeros((sizex, sizey))
+    vbshvegsh = np.zeros((sizex, sizey))
+    vegsh = np.zeros((sizex, sizey))
     f = a
-    g[:] = 0
+    g = np.zeros((sizex, sizey))
     bushplant = bush > 1.
-    pibyfour = np.pi/4.
-    threetimespibyfour = 3.*pibyfour
-    fivetimespibyfour = 5.*pibyfour
-    seventimespibyfour = 7.*pibyfour
+    pibyfour = np.pi / 4.
+    threetimespibyfour = 3. * pibyfour
+    fivetimespibyfour = 5. * pibyfour
+    seventimespibyfour = 7. * pibyfour
     sinazimuth = np.sin(azimuth)
     cosazimuth = np.cos(azimuth)
     tanazimuth = np.tan(azimuth)
     signsinazimuth = np.sign(sinazimuth)
     signcosazimuth = np.sign(cosazimuth)
-    dssin = np.abs((1./sinazimuth))
-    dscos = np.abs((1./cosazimuth))
+    dssin = np.abs((1. / sinazimuth))
+    dscos = np.abs((1. / cosazimuth))
     tanaltitudebyscale = np.tan(altitude) / scale
     index = 1
-    
-    #% main loop
+
+    # % main loop
     while (amaxvalue >= dz and np.abs(dx) < sizex and np.abs(dy) < sizey):
-#         if forsvf == 0:
-#             dlg.progressBar.setValue(index)
-        if (pibyfour <= azimuth and azimuth < threetimespibyfour or fivetimespibyfour <= azimuth and azimuth < seventimespibyfour):
+        #         if forsvf == 0:
+        #             dlg.progressBar.setValue(index)
+        if (
+                pibyfour <= azimuth and azimuth < threetimespibyfour or fivetimespibyfour <= azimuth and azimuth < seventimespibyfour):
             dy = signsinazimuth * index
             dx = -1. * signcosazimuth * np.abs(np.round(index / tanazimuth))
             ds = dssin
@@ -177,73 +177,76 @@ def shadowingfunction_20(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue
             dy = signsinazimuth * np.abs(np.round(index * tanazimuth))
             dx = -1. * signcosazimuth * index
             ds = dscos
-        
-        #% note: dx and dy represent absolute values while ds is an incremental value
+
+        # % note: dx and dy represent absolute values while ds is an incremental value
         dz = np.dot(np.dot(ds, index), tanaltitudebyscale)
         tempvegdem[0:sizex, 0:sizey] = 0.
         tempvegdem2[0:sizex, 0:sizey] = 0.
         temp[0:sizex, 0:sizey] = 0.
         absdx = np.abs(dx)
         absdy = np.abs(dy)
-        xc1 = (dx+absdx)/2.+1.
-        xc2 = sizex+(dx-absdx)/2.
-        yc1 = (dy+absdy)/2.+1.
-        yc2 = sizey+(dy-absdy)/2.
-        xp1 = -((dx-absdx)/2.)+1.
-        xp2 = sizex-(dx+absdx)/2.
-        yp1 = -((dy-absdy)/2.)+1.
-        yp2 = sizey-(dy+absdy)/2.
-        tempvegdem[int(xp1)-1:int(xp2), int(yp1)-1:int(yp2)] = vegdem[int(xc1)-1:int(xc2), int(yc1)-1:int(yc2)]-dz
-        tempvegdem2[int(xp1)-1:int(xp2), int(yp1)-1:int(yp2)] = vegdem2[int(xc1)-1:int(xc2), int(yc1)-1:int(yc2)]-dz
-        temp[int(xp1)-1:int(xp2), int(yp1)-1:int(yp2)] = a[int(xc1)-1:int(xc2), int(yc1)-1:int(yc2)]-dz
+        xc1 = (dx + absdx) / 2. + 1.
+        xc2 = sizex + (dx - absdx) / 2.
+        yc1 = (dy + absdy) / 2. + 1.
+        yc2 = sizey + (dy - absdy) / 2.
+        xp1 = -((dx - absdx) / 2.) + 1.
+        xp2 = sizex - (dx + absdx) / 2.
+        yp1 = -((dy - absdy) / 2.) + 1.
+        yp2 = sizey - (dy + absdy) / 2.
+        tempvegdem[int(xp1) - 1:int(xp2), int(yp1) - 1:int(yp2)] = vegdem[int(xc1) - 1:int(xc2),
+                                                                   int(yc1) - 1:int(yc2)] - dz
+        tempvegdem2[int(xp1) - 1:int(xp2), int(yp1) - 1:int(yp2)] = vegdem2[int(xc1) - 1:int(xc2),
+                                                                    int(yc1) - 1:int(yc2)] - dz
+        temp[int(xp1) - 1:int(xp2), int(yp1) - 1:int(yp2)] = a[int(xc1) - 1:int(xc2), int(yc1) - 1:int(yc2)] - dz
         # f = np.maximum(f, temp) # bad performance in python3. Replaced with fmax
         f = np.fmax(f, temp)
         sh[(f > a)] = 1.
         sh[(f <= a)] = 0.
-        #%Moving building shadow
+        # %Moving building shadow
         fabovea = tempvegdem > a
-        #%vegdem above DEM
+        # %vegdem above DEM
         gabovea = tempvegdem2 > a
-        #%vegdem2 above DEM
+        # %vegdem2 above DEM
         # vegsh2 = np.float(fabovea)-np.float(gabovea)
         vegsh2 = np.subtract(fabovea, gabovea, dtype=float)
         # vegsh = np.maximum(vegsh, vegsh2) # bad performance in python3. Replaced with fmax
         vegsh = np.fmax(vegsh, vegsh2)
-        vegsh[(vegsh*sh > 0.)] = 0.
-        #% removing shadows 'behind' buildings
-        vbshvegsh = vegsh+vbshvegsh
-        #% vegsh at high sun altitudes
+        vegsh[(vegsh * sh > 0.)] = 0.
+        # % removing shadows 'behind' buildings
+        vbshvegsh = vegsh + vbshvegsh
+        # % vegsh at high sun altitudes
         if index == 1.:
-            firstvegdem = tempvegdem-temp
+            firstvegdem = tempvegdem - temp
             firstvegdem[(firstvegdem <= 0.)] = 1000.
             vegsh[(firstvegdem < dz)] = 1.
-            vegsh = vegsh*(vegdem2 > a)
+            vegsh = vegsh * (vegdem2 > a)
             vbshvegsh = np.zeros((sizex, sizey))
 
-        #% Bush shadow on bush plant
-        if np.logical_and(bush.max() > 0., np.max((fabovea*bush)) > 0.):
+        # % Bush shadow on bush plant
+        if np.logical_and(bush.max() > 0., np.max((fabovea * bush)) > 0.):
             tempbush[0:sizex, 0:sizey] = 0.
-            tempbush[int(xp1)-1:int(xp2), int(yp1)-1:int(yp2)] = bush[int(xc1)-1:int(xc2),int(yc1)-1:int(yc2)]-dz
+            tempbush[int(xp1) - 1:int(xp2), int(yp1) - 1:int(yp2)] = bush[int(xc1) - 1:int(xc2),
+                                                                     int(yc1) - 1:int(yc2)] - dz
             # g = np.maximum(g, tempbush) # bad performance in python3. Replaced with fmax
             g = np.fmax(g, tempbush)
             g *= bushplant
         index += 1.
-    
-    sh = 1.-sh
+
+    sh = 1. - sh
     vbshvegsh[(vbshvegsh > 0.)] = 1.
-    vbshvegsh = vbshvegsh-vegsh
-    
+    vbshvegsh = vbshvegsh - vegsh
+
     if bush.max() > 0.:
-        g = g-bush
+        g = g - bush
         g[(g > 0.)] = 1.
         g[(g < 0.)] = 0.
-        vegsh = vegsh-bushplant+g
-        vegsh[(vegsh<0.)] = 0.
-    
+        vegsh = vegsh - bushplant + g
+        vegsh[(vegsh < 0.)] = 0.
+
     vegsh[(vegsh > 0.)] = 1.
-    vegsh = 1.-vegsh
-    vbshvegsh = 1.-vbshvegsh
-    
+    vegsh = 1. - vegsh
+    vbshvegsh = 1. - vbshvegsh
+
     shadowresult = {'sh': sh, 'vegsh': vegsh, 'vbshvegsh': vbshvegsh}
 
     return shadowresult
@@ -266,19 +269,18 @@ def run_skyview_calculations(method_params, tile_name):
     svffolder = OUTPUT_FILE
 
     # the trunk data is not existe, therefore use 25% as default setting in UMEP
-    trunkratio = 0.25 #25/100, from the svf_calculator.py in UMEP
+    trunkratio = 0.25  # 25/100, from the svf_calculator.py in UMEP
 
     ## read the land use file, and judge if we need to process the tile
     # gdal_lu = gdal.Open(lufile)
     # lu_img = gdal_lu.ReadAsArray().astype(np.float)
     # if not np.min(lu_img) < 255: continue
 
-
     with rasterio.open(dsmfile) as dsmlayer:
         dsmimg = dsmlayer.read(1)
 
     gdal_dsm = gdal.Open(dsmfile)
-    dsm = gdal_dsm.ReadAsArray().astype(float)#dsm
+    dsm = gdal_dsm.ReadAsArray().astype(float)  # dsm
     geotransform = gdal_dsm.GetGeoTransform()
     scale = 1 / geotransform[1]
 
@@ -347,55 +349,39 @@ def run_skyview_calculations(method_params, tile_name):
                 iazimuth[index] = iazimuth[index] - 360.
             index = index + 1
 
-
     aziintervalaniso = np.ceil(aziinterval / 2.0)
     index = int(0)
 
-    # WRI Note Initialize arrays that are passed to a function by reference, in order to reduce memory usage
-    sizex = dsmimg.shape[0]
-    sizey = dsmimg.shape[1]
-    ref_temp = np.zeros((sizex, sizey))
-    ref_tempvegdem = np.zeros((sizex, sizey))
-    ref_tempvegdem2 = np.zeros((sizex, sizey))
-    ref_tempbush = np.zeros((sizex, sizey))
-    ref_sh = np.zeros((sizex, sizey))
-    ref_vbshvegsh = np.zeros((sizex, sizey))
-    ref_vegsh = np.zeros((sizex, sizey))
-    ref_g = np.zeros((sizex, sizey))
-    ref_rad_temp = np.zeros((sizex, sizey))
-
-    #for i in np.arange(0, iangle.shape[0]-1):
+    # for i in np.arange(0, iangle.shape[0]-1):
     max_range = skyvaultaltint.shape[0]
     for i in range(0, max_range):
         for j in np.arange(0, (aziinterval[int(i)])):
             altitude = iangle[int(i)]
-            azimuth = iazimuth[int(index)-1]
+            azimuth = iazimuth[int(index) - 1]
 
             # print status
-            if index in (0,145) or (index+1)%20 == 0:
-                print(f"tile: {tile_name}, skyview-step: {index+1}/{len(iazimuth)}, altitude: {altitude}/{iangle.max()}, azimuth: {azimuth}")
+            if index in (0, 145) or (index + 1) % 20 == 0:
+                print(
+                    f"tile: {tile_name}, skyview-step: {index + 1}/{len(iazimuth)}, altitude: {altitude}/{iangle.max()}, azimuth: {azimuth}")
 
             # Casting shadow, when the vegetation dsm is considered
             # if self.usevegdem == 1:
             shadowresult = shadowingfunction_20(dsmimg, vegdem, vegdem2, azimuth, altitude,
-                                                scale, amaxvalue, bush, 1,
-                                                ref_temp, ref_tempvegdem, ref_tempvegdem2, ref_tempbush,
-                                                ref_sh, ref_vbshvegsh, ref_vegsh, ref_g
+                                                scale, amaxvalue, bush, 1
                                                 )
 
             vegsh = shadowresult["vegsh"]
             vbshvegsh = shadowresult["vbshvegsh"]
             vegshmat[:, :, index] = vegsh
 
-            sh = shadowingfunctionglobalradiation(dsmimg, azimuth, altitude, scale, 1, ref_rad_temp)
+            sh = shadowingfunctionglobalradiation(dsmimg, azimuth, altitude, scale, 1)
             shmat[:, :, index] = sh
 
-
             # Calculate svfs
-            k_start = annulino[int(i)]+1
-            k_end = (annulino[int(i+1.)])+1
+            k_start = annulino[int(i)] + 1
+            k_end = (annulino[int(i + 1.)]) + 1
             for k in np.arange(k_start, k_end):
-                weight = annulus_weight(k, aziinterval[i])*sh
+                weight = annulus_weight(k, aziinterval[i]) * sh
                 svf = svf + weight
                 weight = annulus_weight(k, aziintervalaniso[i]) * sh
                 if (azimuth >= 0) and (azimuth < 180):
@@ -410,7 +396,6 @@ def run_skyview_calculations(method_params, tile_name):
                 if (azimuth >= 270) or (azimuth < 90):
                     # weight = self.annulus_weight(k, aziintervalaniso[i])*sh
                     svfN = svfN + weight
-
 
             # WRI Note: Switch to inline variable assignment to reduce memory requirements
             # if self.usevegdem == 1:
@@ -445,7 +430,6 @@ def run_skyview_calculations(method_params, tile_name):
     svfW[(svfW > 1.)] = 1.
     svfN[(svfN > 1.)] = 1.
 
-
     # use the vegetation DSM
     # if self.usevegdem == 1:
     last = np.zeros((rows, cols))
@@ -466,12 +450,10 @@ def run_skyview_calculations(method_params, tile_name):
     svfWaveg[(svfWaveg > 1.)] = 1.
     svfNaveg[(svfNaveg > 1.)] = 1.
 
-
     svfresult = {'svf': svf, 'svfE': svfE, 'svfS': svfS, 'svfW': svfW, 'svfN': svfN,
                  'svfveg': svfveg, 'svfEveg': svfEveg, 'svfSveg': svfSveg, 'svfWveg': svfWveg,
                  'svfNveg': svfNveg, 'svfaveg': svfaveg, 'svfEaveg': svfEaveg, 'svfSaveg': svfSaveg,
-                 'svfWaveg': svfWaveg, 'svfNaveg': svfNaveg, 'shmat': shmat,'vegshmat': vegshmat}
-
+                 'svfWaveg': svfWaveg, 'svfNaveg': svfNaveg, 'shmat': shmat, 'vegshmat': vegshmat}
 
     ## output the svfs into a zip file
     svfs = ['svf', 'svfE', 'svfS', 'svfW', 'svfN', 'svfveg', \
@@ -481,5 +463,5 @@ def run_skyview_calculations(method_params, tile_name):
     for svf in svfs:
         svffile = os.path.join(svffolder, svf + '.tif')
         saverasternd(gdal_dsm, svffile, svfresult[svf])
-        #os.remove(svffile)
+        # os.remove(svffile)
 

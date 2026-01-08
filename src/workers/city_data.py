aeedacba7117b20a183b2ce237a3749026e1fe2a
weiqi-tori <79890935+weiqi-tori@users.noreply.github.com>
2026-01-08 17:25:06 +0800

    Ensure per-tile output folders exist for multi-tile runs

--- src/workers/city_data.py
+++ src/workers/city_data.py
@@ -7,7 +7,7 @@ from shapely import Point
 
 from src.constants import FOLDER_NAME_PRIMARY_DATA, FOLDER_NAME_INTERMEDIATE_DATA, FOLDER_NAME_PRIMARY_MET_FILES, \
     FOLDER_NAME_PRIMARY_RASTER_FILES, FOLDER_NAME_UMEP_TCM_RESULTS, WGS_CRS, FOLDER_NAME_ADMIN_DATA, \
-    FOLDER_NAME_QGIS_DATA
+    FOLDER_NAME_QGIS_DATA, FOLDER_NAME_METADATA
 from src.workers.config_processor import *
 
 
@@ -121,13 +121,14 @@ class CityData:
             obj.target_city_path = str(os.path.join(obj.target_city_parent_path, scenario_sub_folder))
 
             obj.target_city_primary_data_path = str(os.path.join(obj.target_city_path, FOLDER_NAME_PRIMARY_DATA))
-            obj.target_met_files_path = os.path.join(obj.target_city_primary_data_path, FOLDER_NAME_PRIMARY_MET_FILES)
+            obj.target_metadata_path = os.path.join(obj.target_city_path, FOLDER_NAME_METADATA)
+            obj.target_met_files_path = os.path.join(obj.target_metadata_path, FOLDER_NAME_PRIMARY_MET_FILES)
 
-            obj.target_log_path = os.path.join(obj.target_city_path, FOLDER_NAME_ADMIN_DATA)
+            obj.target_log_path = os.path.join(obj.target_metadata_path, FOLDER_NAME_ADMIN_DATA)
             obj.target_manager_log_path = os.path.join(obj.target_log_path, 'log_worker_manager.log')
             obj.target_model_log_path = os.path.join(obj.target_log_path, 'log_model_execution.log')
 
-            obj.target_qgis_data_path = os.path.join(obj.target_city_path, FOLDER_NAME_QGIS_DATA)
+            obj.target_qgis_data_path = os.path.join(obj.target_metadata_path, FOLDER_NAME_QGIS_DATA)
 
             obj.target_intermediate_data_path = os.path.join(obj.target_city_path, FOLDER_NAME_INTERMEDIATE_DATA)
 
@@ -137,9 +138,10 @@ class CityData:
                 metadata_file_name = f'{obj.folder_name_tile_data}_metadata.log'
                 obj.target_umep_metadata_log_path = os.path.join(obj.target_log_path, 'model_metadata', metadata_file_name)
 
-                obj.target_raster_files_path = os.path.join(obj.target_city_primary_data_path,
-                                                            FOLDER_NAME_PRIMARY_RASTER_FILES)
-                obj.target_primary_tile_data_path = os.path.join(obj.target_raster_files_path, obj.folder_name_tile_data)
+                obj.target_tile_path = os.path.join(obj.target_city_path, obj.folder_name_tile_data)
+
+                obj.target_raster_files_path = os.path.join(obj.target_tile_path, FOLDER_NAME_PRIMARY_RASTER_FILES)
+                obj.target_primary_tile_data_path = obj.target_raster_files_path
 
                 obj.target_albedo_cloud_masked_path = os.path.join(obj.target_primary_tile_data_path,
                                                            obj.albedo_cloud_masked_tif_filename)
@@ -150,10 +152,13 @@ class CityData:
                 obj.target_tree_canopy_path = os.path.join(obj.target_primary_tile_data_path,
                                                            obj.tree_canopy_tif_filename)
 
-                obj.target_intermediate_tile_data_path = os.path.join(obj.target_intermediate_data_path, obj.folder_name_tile_data)
+                obj.target_intermediate_data_path = os.path.join(obj.target_tile_path, FOLDER_NAME_INTERMEDIATE_DATA)
+                obj.target_intermediate_tile_data_path = obj.target_intermediate_data_path
                 obj.target_wallheight_path = os.path.join(obj.target_intermediate_tile_data_path, obj.wall_height_filename)
                 obj.target_wallaspect_path = os.path.join(obj.target_intermediate_tile_data_path, obj.wall_aspect_filename)
                 obj.target_svfszip_path = os.path.join(obj.target_intermediate_tile_data_path, obj.skyview_factor_filename)
+
+                obj.target_tcm_results_path = os.path.join(obj.target_tile_path, FOLDER_NAME_UMEP_TCM_RESULTS)
 
         return obj
 

6fe7b143d9e82479010b344388f84a35d84c5b4b
rthngl <56600718+rthngl@users.noreply.github.com>
2026-02-06 17:48:38 +0100

    update file path

--- src/workers/worker_tile_processor.py
+++ src/workers/worker_tile_processor.py
@@ -51,7 +51,7 @@ def process_tile(city_json_str, processing_method, source_base_path, target_base
 
             # make sure there is an output file to write to
             file_stem = Path(met_filename).stem
-            tile_output_path = os.path.join(target_tcm_results_path, file_stem, tile_folder_name)
+            tile_output_path = os.path.join(target_tcm_results_path, file_stem)
             os.makedirs(tile_output_path, exist_ok=True)
 
             if processing_method == 'umep_solweig':
